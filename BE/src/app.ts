78f55351369125d4d0bbcd66c075cb67dfe2a0ef
I585957 <matthew.biju.kavukatt@sap.com>
2024-07-19 12:19:12 +0530

    first request handler

---
+++ BE/src/app.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import notesRoutes from "./routes/notes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes);
+
+app.use((req, res, next) => {
+  next(Error("Endpoint not found"));
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let errorMsg = "Error while fetching notes";
+  if (error instanceof Error) errorMsg = error.message;
+  res.status(500).json({ error: errorMsg });
+});
+
+export default app;
---
+++ BE/src/controllers/notes.ts
@@ -0,0 +1,17 @@
+import { NextFunction, RequestHandler } from "express";
+import NoteModel from "../models/note";
+
+export const getNotes: RequestHandler = async (
+  req,
+  res,
+  next: NextFunction
+) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createNote: RequestHandler = (req, res, next: NextFunction) => {};
