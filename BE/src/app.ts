798362781f0142f5bc569bf2031ba0605f3e6a62
I585957 <matthew.biju.kavukatt@sap.com>
2024-07-19 12:49:32 +0530

    add endpoints to create and fetch individual notes

--- BE/src/app.ts
+++ BE/src/app.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes";
+import morgan from "morgan";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
--- BE/src/controllers/notes.ts
+++ BE/src/controllers/notes.ts
@@ -14,4 +14,27 @@ export const getNotes: RequestHandler = async (
   }
 };
 
-export const createNote: RequestHandler = (req, res, next: NextFunction) => {};
+export const getNote: RequestHandler = async (req, res, next: NextFunction) => {
+  const noteId = req.params.noteId;
+  try {
+    const note = await NoteModel.findById(noteId).exec();
+    return res.status(200).json(note);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createNote: RequestHandler = async (
+  req,
+  res,
+  next: NextFunction
+) => {
+  const title = req.body.title;
+  const text = req.body.text;
+  try {
+    const newNote = await NoteModel.create({ title, text });
+    res.status(201).json(newNote);
+  } catch (error) {
+    next(error);
+  }
+};
